b158fb940cd231747a9a6244dfe81800131a8680
Victor Manuel Hernandez Vargas <hzvvictor@gmail.com>
2022-07-11 21:51:42 -0500

    Segunda version

---
+++ assets/js/components/cart-shop.js
@@ -0,0 +1,39 @@
+import { cartShop } from './cart-elements.js'
+let products = []
+const cartProducts = cartShop.cart.products
+/* 
+ modal carrito => cartShopModal
+*/
+
+const productPrint = () => {
+    /* requiere de template de producto */
+}
+const productAdd = (id, amount) => {
+
+    productPrint()
+}
+const productRemove = (id, amount) => {
+
+}
+const productClear = (id) => {
+
+}
+const cartClear = () => {
+
+}
+const cartbuy = () => {
+    let total
+    
+    return total
+}
+console.log(`cartProducts`);
+console.log(cartProducts);
+export {
+    productPrint,
+    productAdd,
+    productRemove,
+    productClear,
+    cartClear,
+    cartbuy,
+    products
+}
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+'use-strict'
+import {
+    productPrint,
+    productAdd,
+    productRemove,
+    productClear,
+    cartClear,
+    cartbuy,
+    products
+} from './components/cart-shop.js'
+console.log(products);
