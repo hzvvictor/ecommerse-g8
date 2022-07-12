07004d8fbf6e4779a231b4a5884de6220702fb90
WdM2 <degraciawilliams10@gmail.com>
2022-07-12 15:03:10 -0500

    Base de datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,10 @@ import {
     products
 } from './components/cart-shop.js'
 console.log(products);
+
+
+/* base de datos**/
+let productos = [{nombre: 'smartphone', precio:950.00, imagen:'../assets/images/headphone.jpg', catergoria: 'telefono mobile',cantidad:20},
+                {nombre: 'computadora', precio:450.00, imagen:'../assets/images/laptop.png',catergoria: 'laptop',cantidad:30},
+                {nombre: 'Ipad pro',precio:980, imagen:'../assets/images/ipad-pro.jpeg', catergoria: 'tablet',cantidad:10},
+                {nombre: 'audifonos', precio:50.00, imagen:'../samsung-phone.png',catergoria:'asesorios',cantidad:15}]
