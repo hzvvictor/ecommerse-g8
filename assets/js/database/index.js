fbf0d68520c638c64d1638b954ac1f0d00578745
WdM2 <degraciawilliams10@gmail.com>
2022-07-12 16:48:15 -0500

    database

---
+++ assets/js/database/index.js
@@ -0,0 +1,34 @@
+
+let productos = [
+    {
+    id:1, 
+    nombre: 'smartphone', 
+    precio:950.00, imagen:'../../images/headphone.jpg', 
+    catergoria: 'telefono mobile',
+    cantidad:20
+},
+{
+    id:2, 
+    nombre: 'computadora', 
+    precio:450.00, imagen:'../../images/laptop.png',
+    catergoria: 'laptop',
+    cantidad:30
+},
+{
+    id:3, 
+    nombre: 'Ipad pro',
+    precio:980, imagen:'../../images/ipad-pro.jpeg', 
+    catergoria: 'tablet',
+    cantidad:10
+},
+{
+    id:4, 
+    nombre: 'audifonos', 
+    precio:50.00, 
+    imagen:'../../images/samsung-phone.png',
+    catergoria:'asesorios',
+    cantidad:15
+}
+]
+
+export default productos
