8f4a240e7b2fe271c3baf03c684e5197d411b24a
Victor Manuel Hernandez Vargas <hzvvictor@gmail.com>
2022-07-12 20:34:59 -0500

    Se agrego pintar

--- assets/js/components/cart-shop.js
+++ assets/js/components/cart-shop.js
@@ -1,12 +1,45 @@
 import { cartShop } from './cart-elements.js'
-let products = []
+let ec = []
+let productsSelect = []
 const cartProducts = cartShop.cart.products
+
 /* 
  modal carrito => cartShopModal
 */
 
 const productPrint = () => {
     /* requiere de template de producto */
+    let inner = ''
+    products.forEach(({ id, title, category, img, price, stock }) => {
+        const template =
+            `<section id="prod__card:${id}" data-category="${category}" class="prod__card card-scrolRevel">
+    <figure class="prod__img">
+        <img src="${img}" alt="" class="img_prod" id="img_prod">
+    </figure>
+
+    <content class="prod__inf">
+        <h4>${title}</h4>
+        <div class="prod__decription">
+            <span class="prod__price">${price}</span>
+
+            <span class="prod__stock">
+                | Stock:
+                <span>${stock}</span>
+            </span>
+        </div>
+    </content>
+    <div class="card__buy">
+        <div class="prod__amount">
+            <span class="amount__btn remove"><i class='bx bx-minus'></i></span>
+            <span id="cant_prod">1</span>
+            <span class="amount__btn add"><i class='bx bx-plus'></i></span>
+        </div>
+        <span class="btn_buy" id="btn_buy">Buy</span>
+    </div>
+</section>`
+        inner += template
+    })
+    cartProducts.innerHTML = inner
 }
 const productAdd = (id, amount) => {
 
@@ -14,12 +47,15 @@ const productAdd = (id, amount) => {
 }
 const productRemove = (id, amount) => {
 
+    productPrint()
 }
+
 const productClear = (id) => {
 
 }
 const cartClear = () => {
-
+    productsSelect = []
+    cartProducts.innerHTML = ''
 }
 const cartbuy = () => {
     let total
