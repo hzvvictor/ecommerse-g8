7a5a4d205967f1bff043ec8fe7b41524122593ad
Jmontenar <jmontenar@gmail.com>
2022-07-12 21:29:01 -0500

    Base de datos

--- assets/js/database/index.js
+++ assets/js/database/index.js
@@ -2,33 +2,54 @@
 let productos = [
     {
     id:1, 
-    nombre: 'smartphone', 
-    precio:950.00, imagen:'../../images/headphone.jpg', 
-    catergoria: 'telefono mobile',
+    nombre: 'Samsung', 
+    precio:950.00, imagen:'../../images/samsung-phone.png', 
+    categoria: 'telefono mobile',
     cantidad:20
 },
 {
     id:2, 
-    nombre: 'computadora', 
-    precio:450.00, imagen:'../../images/laptop.png',
-    catergoria: 'laptop',
-    cantidad:30
+    nombre: 'Iphone 13', 
+    precio:12360.00, imagen:'../../images/iphone-13-pro-.jpg', 
+    categoria: 'telefono mobile',
+    cantidad:20
 },
 {
     id:3, 
-    nombre: 'Ipad pro',
-    precio:980, imagen:'../../images/ipad-pro.jpeg', 
-    catergoria: 'tablet',
-    cantidad:10
+    nombre: 'Samsung Galaxy S8', 
+    precio:350.00, imagen:'../../images/GalaxyS8_PCD.jpg', 
+    categoria: 'telefono mobile',
+    cantidad:20
 },
 {
     id:4, 
+    nombre: 'Portatil Huawei', 
+    precio:480.00, imagen:'../../images/huawei portatil.png',
+    categoria: 'laptop',
+    cantidad:30
+},
+{
+    id:5, 
+    nombre: 'Portatil HP',
+    precio:595, imagen:'../../images/portatil hp.jpg', 
+    categoria: 'laptop',
+    cantidad:10
+},
+{
+    id:6, 
     nombre: 'audifonos', 
     precio:50.00, 
     imagen:'../../images/samsung-phone.png',
     catergoria:'asesorios',
     cantidad:15
-}
+},
+{
+    id:7, 
+    nombre: 'Air Pods',
+    precio:595, imagen:'../../images/airpods.jpg', 
+    categoria: 'accesorios',
+    cantidad:10
+},
 ]
 
 export default productos
